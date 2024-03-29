6676c53e8349351a519f8c41311251520921e0b6
ViniciusQuatrin <viniquatrin48@gmail.com>
2024-03-29 16:16:02 -0300

    added appointments, exams and some constants

---
+++ db/migrate/20240329181408_create_exams.rb
@@ -0,0 +1,11 @@
+class CreateExams < ActiveRecord::Migration[7.1]
+  def change
+    create_table :exams do |t|
+      t.references :patient, null: false, foreign_key: true
+      t.string :name
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
