06c95563b8bbc28a07fdcaac39aa49180a7eb071
ViniciusQuatrin <viniquatrin48@gmail.com>
2024-03-30 15:42:16 -0300

    update controllers and routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  namespace :api do
+    namespace :v1 do
+      resources :users
+      resources :questions
+      resources :doctors
+    end
+  end
 end
